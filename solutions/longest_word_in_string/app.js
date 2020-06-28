4baf26fe9b4202af0ce07c757d8e47594fe9eb00
Donald Brown <donbrown4652@gmail.com>
2020-06-28 18:27:36 -0500

    adds updateUI to find longest word

--- solutions/longest_word_in_string/app.js
+++ solutions/longest_word_in_string/app.js
@@ -9,5 +9,18 @@ function findLongestWordLength(str) {
   return longest;
 }
 
+function upDateUI(){
+  let string = document.getElementById('getStr').value;
+  let message = '';
+  if(string == ""){
+    message = 'You need to enter a string first';
+  }else{
+    message = `The longest word is ${findLongestWordLength(string)} characters long`;
+  }
+  document.getElementById('display').innerHTML = message;
+}
+document.getElementById('btn').addEventListener('click', upDateUI);
+
+
 console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog")); //logs 6
 console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology")); // logs 19
